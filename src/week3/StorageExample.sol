7c35920b08ae21251d222c7034c17753cc91e79c
Misbahu Abubakar Abdullahi <94783350+Misbah-Engr@users.noreply.github.com>
2026-02-21 10:45:54 +0100

    Add StorageExample contract with state variables

---
+++ src/week3/StorageExample.sol
@@ -0,0 +1,6 @@
+contract Example {
+    uint256 public a;       // Slot 0 (full 32 bytes)
+    address public owner;   // Slot 1 (20 bytes, starts at byte 0)
+    bool public paused;     // Slot 1 (1 byte, starts at byte 20, packed with owner)
+    uint256 public b;       // Slot 2
+}
