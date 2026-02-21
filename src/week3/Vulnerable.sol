b8ce12f99b6fb7c092ebfc507c9133d9d79c2989
Misbahu Abubakar Abdullahi <94783350+Misbah-Engr@users.noreply.github.com>
2026-02-21 12:36:55 +0100

    Add status variable to Vulnerable contract
    Added a status variable to the Vulnerable contract.

--- src/week3/Vulnerable.sol
+++ src/week3/Vulnerable.sol
@@ -1,5 +1,6 @@
 contract Vulnerable {
     address public owner;         // Slot 0
+    bool public status: slot 0
     uint256 public totalDeposits; // Slot 1
 
     function updateOwner(address newOwner) external {
